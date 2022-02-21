1673b1ea9517354a26e9b728ae4bac9221b28a5d
alexfigas <alexfigas11@gmail.com>
2022-02-21 21:09:19 +0000

    Small adjustments

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,14 +7,14 @@ import videos from '../videos/welcome.mp4'
 function Welcome() {
   return (
     <div className='welcome-container'>
-        {/* Video */}
-        <video src={videos} autoPlay loop muted />
-        <h1>WELCOME</h1>
-        <p>AlexFigas.github.io</p>
-        <div className="welcome-btns">
-            <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>ABOUT ME</Button>
-            <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>GITHUB</Button>
-        </div>
+      {/* Video */}
+      <video src={videos} autoPlay loop muted />
+      <h1>WELCOME</h1>
+      <p>alexfigas.github.io</p>
+      <div className="welcome-btns">
+        <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>ABOUT ME</Button>
+        <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>GITHUB</Button>
+      </div>
     </div>
   )
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,9 @@ import '../../App.css'
 import Welcome from '../Welcome'
 
 function Home() {
-    return(
+    return (
         <>
-            <Welcome/>
+            <Welcome />
         </>
     );
 }
