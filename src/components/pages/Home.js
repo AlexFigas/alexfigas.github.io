b3e0791fd145401bea0e6784d4c813272e5b8604
alexfigas <alexfigas11@gmail.com>
2022-02-22 12:35:02 +0000

    Simple card component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css'
+import Cards from '../Cards';
 import Welcome from '../Welcome'
 
 function Home() {
     return (
         <>
             <Welcome />
+            <Cards />
         </>
     );
 }
